4787d837dbf14776fcab146152cb1df402e042fa
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-10-26 12:50:59 +0200

    initial commit

---
+++ src/Market/InfoMarket.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file InfoMarket.php
+ * Provides the InfoMarket core class
+ * Lang en
+ * Reviewstatus: 2021-10-26
+ * Localization: none
+ * Documentation: complete
+ * Tests: 
+ * Coverage: unknown
+ * Dependencies: none
+ * PSR-State: complete
+ */
+
+namespace Sunhill\InfoMarket\Market;
+
+class InfoMarket
+{
+  
+  
+}
