5e7cd6bcae78534300eaeef2cf8e04a50a0aeb35
Klaus Dimde <82110737+sunhill-de@users.noreply.github.com>
2021-11-04 12:53:46 +0100

    moved FakeMarketeer into its one file

--- tests/Unit/Marketeers/MarketeersTest.php
+++ tests/Unit/Marketeers/MarketeersTest.php
@@ -6,59 +6,7 @@ use Sunhill\InfoMarket\Test\InfoMarketTestCase;
 use Sunhill\InfoMarket\Marketeers\MarketeerBase;
 use Sunhill\InfoMarket\Marketeers\Response\Response;
 use Sunhill\InfoMarket\Marketeers\MarketeerException;
-
-class FakeMarketeer extends MarketeerBase
-{
-    protected function getOffering(): array
-    {
-        return [
-            'test.item'=>'getTestItem',
-            'test.catchall'=>'getTestCatchall',
-            'test.array.*.item'=>'getTestArray',
-            'another.*.array.*'=>'getAnotherArray'
-        ];
-    }
-    
-    protected function itemIsReadable(string $name): bool
-    {
-        return true;        
-    }
-    
-    protected function itemIsWriteable(string $name): bool
-    {
-        return false;    
-    }
-    
-    protected function getItemResponse(string $name): Response
-    {
-        if ($name == 'test.catchall') {
-            return $this->getCatchall();
-        } else {
-            return parent::getItemResponse($name);
-        }
-    }
-    
-    protected function getTestItem(): Response
-    {
-        
-    }
-    
-    protected function getTestCatchall(): Response
-    {
-        
-    }
-    
-    protected function getTestArray($index): Response
-    {
-        
-    }
-    
-    protected function getAnotherArray($index1,$index2): Response
-    {
-        
-    }
-    
-}
+use Sunhill\InfoMarket\Test\Marketeers\FakeMarketeer;
 
 class MarketeersTest extends InfoMarketTestCase
 {
